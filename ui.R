5d8ef7920bc1a909eab87945ef7e2d5e4b2d25e6
bsugg <sugg.brian@gmail.com>
2020-07-22 16:35:37 -0400

    Setup sidebar menu structure

--- ui.R
+++ ui.R
@@ -3,12 +3,25 @@ library(ggplot2)
 
 dashboardPage(
   dashboardHeader(title = "College Football"),
+  
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Teams", tabName = "teams", icon = icon("th")),
-      menuItem("Venues", tabName = "venues", icon = icon("th"))
+      menuItem("Information", tabName = "info", icon = icon("th")),
+      menuItem("Data Exploration", tabName = "explore", icon = icon("th"),
+                menuSubItem("Teams", tabName = "teams"),
+                menuSubItem("Games", tabName = "games"),
+                menuSubItem("Venues", tabName = "venues")
+               ),
+      menuItem("Unsupervised Learning", tabName = "unsupervised", icon = icon("th")),
+      menuItem("Modeling", tabName = "model", icon = icon("th"),
+                menuSubItem("Model 1", tabName = "model1"),
+                menuSubItem("Model 2", tabName = "model2")
+               ),
+      menuItem("Data", tabName = "data", icon = icon("th")),
+      menuItem("Source API", icon = icon("file-code-o"),href = "https://collegefootballdata.com/")
     )
   ),
+  
   dashboardBody(
     tabItems(
       # First tab content
