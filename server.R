61a9a9c53a90b1a95f74e601fd9ee9eea1c6144c
bsugg <sugg.brian@gmail.com>
2020-07-22 18:32:45 -0400

    Update with menu icons and start of body structure

--- server.R
+++ server.R
@@ -4,8 +4,17 @@ library(ggplot2)
 
 load("collegeFootball.Rdata")
 
-
 function(input, output, session) {
+  
+  teamsNew <- reactive({
+    teams <- teams %>% filter(school == input$team)
+  })
+  
+  gamesNew <- reactive({
+    games <- bind_rows(filter(games,home_team == input$team),
+                       filter(games,away_team == input$team)
+                      )
+  })
   
   teamsR <- reactive({teams})
   venuesR <- reactive({venues})
@@ -13,12 +22,5 @@ function(input, output, session) {
   output$tableTeams <- renderTable({
     teamsR()
   })
-  
-  
-  output$tableVenues <- renderTable({
-    venuesR()
-  })
-
-  
 
 }
--- ui.R
+++ ui.R
@@ -1,42 +1,98 @@
 library(shinydashboard)
 library(ggplot2)
 
+# Shinydashboard information here https://rstudio.github.io/shinydashboard/index.html
+# Icons sourced from https://fontawesome.com/icons?d=gallery&m=free
+
+load("collegeFootball.Rdata")
+
 dashboardPage(
-  dashboardHeader(title = "College Football"),
+  
+  #
+  ##
+  ### HEADER
+  ##
+  #
+  
+  dashboardHeader(title = "NCAA College Football",titleWidth = 250),
+  
+  #
+  ##
+  ### SIDEBAR
+  ##
+  #
   
   dashboardSidebar(
+    width = 250,
     sidebarMenu(
-      menuItem("Information", tabName = "info", icon = icon("th")),
-      menuItem("Data Exploration", tabName = "explore", icon = icon("th"),
+      menuItem("Information", tabName = "info", icon = icon("info")),
+      menuItem("Data Exploration", tabName = "explore", icon = icon("bar-chart-o"),
                 menuSubItem("Teams", tabName = "teams"),
                 menuSubItem("Games", tabName = "games"),
                 menuSubItem("Venues", tabName = "venues")
                ),
-      menuItem("Unsupervised Learning", tabName = "unsupervised", icon = icon("th")),
-      menuItem("Modeling", tabName = "model", icon = icon("th"),
+      menuItem("Unsupervised Learning", tabName = "unsupervised", icon = icon("chalkboard-teacher")),
+      menuItem("Modeling", tabName = "model", icon = icon("code-branch"),
                 menuSubItem("Model 1", tabName = "model1"),
                 menuSubItem("Model 2", tabName = "model2")
                ),
       menuItem("Data", tabName = "data", icon = icon("th")),
-      menuItem("Source API", icon = icon("file-code-o"),href = "https://collegefootballdata.com/")
+      menuItem("Source API", icon = icon("file-code-o"),href = "https://collegefootballdata.com/"),
+      selectizeInput("team", "Team",
+                  selected = "North Carolina", choices = levels(as.factor(teams$school))
+      )
     )
   ),
   
+  #
+  ##
+  ### BODY
+  ##
+  #
+  
   dashboardBody(
     tabItems(
-      # First tab content
-      tabItem(tabName = "teams",
+      
+      # Information
+      tabItem(tabName = "info",
               fluidRow(
-                box(tableOutput("tableTeams"))
+                box(title = "ST558 - Project 3", status = "primary",
+                  "Brian Sugg", br(),
+                  "July 27, 2020", br(),
+                  br(),
+                  "Describes the data and abilities of the app.", br(),
+                  br(),
+                  "Utilize HEADERS and FORMAT TEXT."
+                )
               )
       ),
       
-      # Second tab content
+      # Data Exploration - Teams
+      tabItem(tabName = "teams",
+              fluidRow(
+                column(width=9,
+                  box(title = "Team Details", status = "primary", width=9,
+                      "Some text here and here and here..."
+                  )
+                )
+              )
+      ),
+      
+      # Data Exploration - Games
+      tabItem(tabName = "games",
+              fluidRow(
+                box(tableOutput("tableVenues"))
+              )
+      ),
+      
+      # Data Exploration - Venues
       tabItem(tabName = "venues",
               fluidRow(
                 box(tableOutput("tableVenues"))
               )
       )
+      
+      # Next one here
     )
   )
 )
